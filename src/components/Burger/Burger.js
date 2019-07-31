f98a9121b6ca08f7e7512eedda5fa319d9aa7d64
Andre Ramos <andre.lopes@soaexpert.com.br>
2019-07-30 23:48:57 -0300

    Burger controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,24 @@ import classes from './Burger.module.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients).map((igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, index) => {
+      return <BurgerIngredient key={igKey + index} type={igKey} />
+    })
+  })).reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding some ingredients!</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
