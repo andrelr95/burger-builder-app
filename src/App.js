be8ee38e2114cd863140fa2584c263aa8a96afab
Andre Ramos <andre.lopes@soaexpert.com.br>
2019-08-01 10:42:45 -0300

    Backdrop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuild';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 import classes from './Modal.module.scss';
+import Lux from '../../../hoc/Lux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const Modal = (props) => (
-  <div 
-    className={classes.Modal}
-    style={{ 
-      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}
-    >
-    {props.children}
-  </div>
+  <Lux>
+    <Backdrop clicked={props.modalClosed} show={props.show} />
+    <div 
+      className={classes.Modal}
+      style={{ 
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+      >
+      {props.children}
+    </div>
+  </Lux>
 )
 
 export default Modal;
